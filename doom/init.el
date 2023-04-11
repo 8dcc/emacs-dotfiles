9cc371d9ddc1d6ddac3f8d5a41d11c9e28e64de2
8dcc <8dcc.git@gmail.com>
2023-04-11 15:14:09 +0200

    Enable emms module

--- doom/init.el
+++ doom/init.el
@@ -183,7 +183,7 @@
 
        :app
        ;;calendar
-       ;;emms
+       emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
