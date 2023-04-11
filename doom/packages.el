87a5f1a2b4a2c107064d3f87b48ab83c77ae8835
8dcc <8dcc.git@gmail.com>
2023-04-11 23:17:09 +0200

    Pin packages to their latest commits

--- doom/packages.el
+++ doom/packages.el
@@ -2,11 +2,11 @@
 ;;; $DOOMDIR/packages.el
 
 ;; Nasm support
-(package! nasm-mode)
+(package! nasm-mode :pin "824d11fdceb01dca9ae2757670ee1f24ca2477de")
 
 ;; Nov.el (For epubs). Uses doom-variable-pitch-font
-(package! nov)
+(package! nov :pin "6c992c2c5c4ad03a0f432a74fe4d0cde3b6da4bd")
 
 ;; Generate doxygen documentation from C code
-(package! gendoxy
+(package! gendoxy :pin "824d11fdceb01dca9ae2757670ee1f24ca2477de"
   :recipe (:host github :repo "mp81ss/gendoxy"))
