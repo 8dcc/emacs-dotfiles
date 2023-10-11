e418683e6441ba3239705300dd61028fda573f0b
8dcc <8dcc.git@gmail.com>
2023-10-11 14:16:27 +0200

    Change splash image and menu entries
    Add theme section in config.el
    Move split settings to right section (org -> general)
    Change format of separator comments
    Remove battery alarm TODO comment

--- doom/config.el
+++ doom/config.el
@@ -1,12 +1,12 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; ------------------------ FUNCTIONS ------------------------
+;; --- MISC FUNCTIONS ----------------------------------------------------------
 
 ;; $DOOMDIR/custom-lisp/custom-functions.el
 (add-to-list 'load-path (concat doom-user-dir "custom-lisp/"))
 (require 'custom-functions)
 
-;; ------------------------ GENERAL SETTINGS ------------------------
+;; --- GENERAL SETTINGS --------------------------------------------------------
 
 ;; Identification for GPG configuration, email, templates...
 (setq user-full-name "8dcc"
@@ -17,15 +17,13 @@
 ;;  * media-fonts/fira-sans
 ;;  * media-fonts/fira-code
 (setq doom-font (font-spec :family "Dina" :size 10)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :weight 'light :size 14)
+      doom-variable-pitch-font (font-spec :family "Fira Sans"
+                                          :weight 'light :size 14)
       doom-big-font-increment 5)
 (custom-set-faces
   '(org-level-1 ((t (:inherit outline-1 :family "Fira Code" :height 1.6))))
   '(org-level-2 ((t (:inherit outline-2 :family "Fira Code" :height 1.2))))
   '(org-level-3 ((t (:inherit outline-3 :family "Fira Code" :height 1.2)))))
-
-;; Theme
-(setq doom-theme 'doom-monokai-custom)
 
 ;; Style of line numbers. If set to `nil', line numbers are disabled, `t' for
 ;; normal numbers and `relative' for relative line numbers.
@@ -39,10 +37,52 @@
 (add-hook! 'prog-mode-hook
   (setq display-fill-column-indicator-character ?\u00A6))
 
-;; ------------------------ BATTERY ------------------------
+;; Split to the right and bellow
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
 
-;; Show battery in mode line. If the battery is "N/A", don't display.
-;; TODO: (add-hook! 'battery-update-functions #'my-battery-alarm)
+;; --- THEME -------------------------------------------------------------------
+
+;; Custom monokai inside $DOOMDIR/themes/
+(setq doom-theme 'doom-monokai-custom)
+
+;; Change splash screen image
+(setq fancy-splash-image (concat doom-user-dir "img/splash.png"))
+
+;; Splash screen entries
+(setq +doom-dashboard-menu-sections
+      '(("Reload last session"
+         :icon (all-the-icons-octicon "history"
+                                      :face 'doom-dashboard-menu-title)
+         :when (cond ((modulep! :ui workspaces)
+                      (file-exists-p
+                       (expand-file-name persp-auto-save-fname persp-save-dir)))
+                     ((require 'desktop nil t)
+                      (file-exists-p (desktop-full-file-name))))
+         :face (:inherit (doom-dashboard-menu-title bold))
+         :action doom/quickload-session)
+        ("Recently opened files"
+         :icon (all-the-icons-octicon "file-text"
+                                      :face 'doom-dashboard-menu-title)
+         :action recentf-open-files)
+        ("Open project"
+         :icon (all-the-icons-octicon "briefcase"
+                                      :face 'doom-dashboard-menu-title)
+         :action projectile-switch-project)
+        ("Open org-agenda"
+         :icon (all-the-icons-octicon "calendar"
+                                      :face 'doom-dashboard-menu-title)
+         :when (fboundp 'org-agenda)
+         :action org-agenda)
+        ("Open private configuration"
+         :icon (all-the-icons-octicon "tools"
+                                      :face 'doom-dashboard-menu-title)
+         :when (file-directory-p doom-user-dir)
+         :action doom/open-private-config)))
+
+;; --- BATTERY -----------------------------------------------------------------
+
+;; Show battery in mode line. If the battery is "N/A" or "unknown", don't display.
 (require 'battery)
 (after! battery
   (let ((battstr (battery-format "%B" (funcall battery-status-function))))
@@ -51,11 +91,7 @@
         (display-battery-mode 0)
       (display-battery-mode 1))))
 
-;; ------------------------ ORG ------------------------
-
-;; Split to the right and bellow
-(setq evil-split-window-below t
-      evil-vsplit-window-right t)
+;; --- ORG ---------------------------------------------------------------------
 
 ;; Org agenda location
 (setq org-directory (expand-file-name "~/Sync/Org/"))
@@ -73,7 +109,7 @@
 ;; Enable "<s TAB" keys for src blocks
 (require 'org-tempo)
 
-;; ------------------------ IRC ------------------------
+;; --- IRC ---------------------------------------------------------------------
 
 (require 'erc)
 (require 'erc-log)
@@ -151,7 +187,7 @@
            :port     "6697"
            :password erc-pass))
 
-;; ------------------------ REMAPS ------------------------
+;; --- REMAPS ------------------------------------------------------------------
 
 ;; :q -> SPC b k
 (map! [remap evil-quit] #'kill-current-buffer)
@@ -185,7 +221,7 @@
       :map org-mode-map
       :desc "[Custom] Inline images" :n "SPC t i" #'org-toggle-inline-images)
 
-;; ------------------------ EMMS ------------------------
+;; --- EMMS --------------------------------------------------------------------
 
 ;; For streaming from libre.fm using emms
 (require 'emms-librefm-stream)
@@ -199,7 +235,7 @@
 
 (emms-mode-line-mode 0)         ; Only display time, not song
 
-;; ------------------------ MAIL ------------------------
+;; --- MAIL --------------------------------------------------------------------
 
 (require 'smtpmail)
 (setq send-mail-function   'smtpmail-send-it
@@ -207,7 +243,7 @@
       smtpmail-stream-type 'ssl
       smtpmail-smtp-service 465)
 
-;; ------------------------ CC-MODE ------------------------
+;; --- CC-MODE -----------------------------------------------------------------
 
 ;; Enable literal tabs for C code (if not on beginning of line)
 (setq c-default-style "k&r"
