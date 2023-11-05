b369b50e6547c1ed8798e3b1a69e943db9f52d14
8dcc <8dcc.git@gmail.com>
2023-11-05 17:00:33 +0100

    Update custom.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0e59bfd38be132d3c1f85e7de1f8c65c3fb5f3819425dcfe9b61b2efdf918ed3" default))
+   '("d91aa6a67c0dd2be2046d17d6cf2112c8fdc5d75c1ce4c098feb928e24c08c0a" default))
  '(package-selected-packages '(which-key evil-collection)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
