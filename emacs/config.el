108d7f422f7fb5c786a3555a1f4fdfee7e33173e
8dcc <8dcc.git@gmail.com>
2023-11-07 23:23:29 +0100

    Change magit-display-buffer-function

--- emacs/config.el
+++ emacs/config.el
@@ -112,17 +112,7 @@
 (use-package magit
   :config
   (setq magit-display-buffer-function
-        (lambda (buffer)
-          (display-buffer
-           buffer (if (and (derived-mode-p 'magit-mode)
-                           (memq (with-current-buffer buffer major-mode)
-                                 '(magit-process-mode
-                                   magit-revision-mode
-                                   magit-diff-mode
-                                   magit-stash-mode
-                                   magit-status-mode)))
-                      nil
-                    '(display-buffer-same-window))))))
+        #'magit-display-buffer-same-window-except-diff-v1))
 
 (use-package dashboard
   :init
