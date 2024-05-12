f5f0dc925429d1148b99cdfdb19c961b617e6aef
8dcc <8dcc.git@gmail.com>
2024-05-12 17:41:08 +0200

    Remove TXL mode

--- emacs/config.el
+++ emacs/config.el
@@ -388,12 +388,6 @@ and ALIGNMENT as parameters."
       '("\\.gpr\\'" "\\.ada\\'" "\\.ads\\'" "\\.adb\\'"))
 
 (straight-use-package
- '(txl-mode :type git :host github :repo "8dcc/txl-mode.el"))
-
-(add-to-list 'auto-mode-alist
-             '("\\.\\([tT]xl\\|[gG]rm\\|[gG]rammar\\|[rR]ul\\(es\\)?\\|[mM]od\\(ule\\)?\\)$" . txl-mode))
-
-(straight-use-package
  '(nasm-mode :type git :host github :repo "8dcc/nasm-mode"))
 
 (add-to-list 'auto-mode-alist '("\\.asm\\'"  . nasm-mode))
