2a171f28022461763450bc1774d0c5d6c7ec7059
8dcc <8dcc.git@gmail.com>
2024-06-16 14:52:26 +0200

    Add "SPC f F" keybind

--- emacs/config.el
+++ emacs/config.el
@@ -526,6 +526,7 @@ With argument ARG, do this that many times."
   "f c" '(compile            :wk "Compile")
   "f C" '(recompile          :wk "Re-compile")
   "f f" '(find-file          :wk "Find file")
+  "f F" '(find-name-dired    :wk "Find wildcard recursively")
   "f o" '(ff-find-other-file :wk "Find other file")
   "f O" '(find-file-at-point :wk "Find file at point")
   "f r" '(recentf-open       :wk "Open recent")
