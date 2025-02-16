dd7862ea8883f4c7456a3de547fd69e2fa1d0884
8dcc <8dcc.git@gmail.com>
2025-02-16 13:11:05 +0100

    Enable 'flycheck-mode' in C, SH and Elisp modes

--- emacs/config.el
+++ emacs/config.el
@@ -352,6 +352,7 @@ Using `x8dcc/general-create-definer'."
         languagetool-server-command "org.languagetool.server.HTTPServer"))
 
 (use-package flycheck
+  :hook ((c-mode-common sh-mode emacs-lisp-mode) . flycheck-mode)
   :config
   (setq flycheck-check-syntax-automatically
         '(mode-enabled save idle-buffer-switch))
