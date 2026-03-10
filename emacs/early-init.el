940f7fbff87c3efbb6b0dc3c3db04409bac3eb0b
8dcc <8dcc.git@gmail.com>
2026-03-10 17:00:01 +0100

    Increase 'gc-cons-threshold' in 'eary-init.el'

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,4 +1,10 @@
+;; -----------------------------------------------------------------------------
+;; Early init file for 8dcc's Emacs.
+;; See: https://github.com/8dcc/emacs-dotfiles
+;; -----------------------------------------------------------------------------
+
+;; Increase the garbage collection threshold to improve startup times.
+(setq gc-cons-threshold (* 100 1024 1024))
 
 ;; Disable package.el (for straight.el)
 (setq package-enable-at-startup nil)
-
