745e3b0f9e2b65129d88b7b083342799e2504148
8dcc <8dcc.git@gmail.com>
2026-08-14 11:48:37 +0200

    Bring back agenda section from dashboard
    This reverts commit 65dc763b109649c8816eeeca9a5a679bf278878c.

--- emacs/config.el
+++ emacs/config.el
@@ -289,7 +289,8 @@ Using `x8dcc/general-create-definer'."
         dashboard-set-footer nil
         dashboard-page-separator "\n\n"
         dashboard-items '((recents . 10)
-                          (projects . 5)))
+                          (projects . 5)
+                          (agenda . 10)))
   :config
   (dashboard-setup-startup-hook))
 
